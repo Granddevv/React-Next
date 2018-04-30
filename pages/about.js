262607a7b196090e4b1cdd932e2a2d2d600fd0ea
granddevv <spacingflight@outlook.com>
2018-04-30 22:09:22 +0800

    Add Link to go to the next pages

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 export default () => (
     <div>
         <p>About.js</p>
+        <Link href="/next" style={{fontSize: 20}}>
+            <button> Goto Next Page </button>
+        </Link>
     </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 const Index = () => (
     <div>
+        <div>
+            <Link href="/about">Goto About Page</Link>
+        </div>
         <p>Hello Next.js</p>
     </div>
 )
