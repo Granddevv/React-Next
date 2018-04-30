f88e0f7ab689cc134b4359cfbe2975681786698f
granddevv <spacingflight@outlook.com>
2018-04-30 22:09:09 +0800

    Add Link to go to the next pages

---
+++ pages/next.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <p> Next page </p>
+        <Link href="/thenext"> The Next Link </Link>
+    </div>
+)
---
+++ pages/thenext.js
@@ -0,0 +1,5 @@
+export default () => (
+    <div>
+        <p> The Next Page </p>
+    </div>
+)
