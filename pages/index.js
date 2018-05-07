448bd4bbd6879bf4b3fbb261ffa49a8621a2ed2e
granddevv <spacingflight@outlook.com>
2018-05-08 02:42:31 +0800

    Add getInitialProps function for fetching data from apis

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
-import Link from 'next/link';
-import Header from '../comps/Header';
-import Layout from '../comps/Layout';
+import Layout from '../comps/Layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a> {props.title} </a>
-        </Link>
-    </li>
-)
-
-
-const Index = () => (
+const Index = (props) => (
     <Layout>
-        <p> Hello My blog</p>
+        <h1>Batman TV Shows</h1>
         <ul>
-            <PostLink id="home" title='Home Home.js' />
-            <PostLink id="about" title='About About.js' />
-            <PostLink id="next" title='Next Next.js' />
-            <PostLink id="thenext" title='The Next JS' />
+            {props.shows.map(({show}) => (
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
         </ul>
     </Layout>
 )
 
-export default Index;
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    console.log(`Show data fetched. Count: ${data.length}`)
+
+    return {
+        shows: data
+    }
+}
+
+export default Index
