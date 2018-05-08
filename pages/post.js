8ab4a29b471e28bcd89e07db1368c098c5d55109
granddevv <spacingflight@outlook.com>
2018-05-08 10:45:32 +0800

    Add item fetch funcitonality, Add start, dev functionality

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,18 @@
 import Layout from '../comps/Layout';
 
-export default (props) => (
+const Post = (props) => (
     <Layout>
-        <h1> {props.url.query.title} </h1>
-        <p> Here is post Page </p>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium}/>
     </Layout>
 )
+
+Post.getInitialProps = async function(context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+    return { show };
+}
+
+export default Post
